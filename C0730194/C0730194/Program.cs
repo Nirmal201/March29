31758244c6ce327ac52b30b880f12c0349713c16
visualstudio <visualstudio@122-PC26>
2019-03-29 19:15:46 -0400

    Commit 1

---
+++ C0730194/C0730194/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0730194
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld hw = new HelloWorld();
+
+        }
+    }
+    class HelloWorld {
+        public void hello()
+        {
+            Console.ReadLine("Hello World");
+        }
+    }
+}
